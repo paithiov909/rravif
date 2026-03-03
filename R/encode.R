9a7683924ef72b31c229a17047f86615d21a14c5
paithiov909 <am11.44@gmail.com>
2026-03-03 14:16:04 +0900

    add 'write_avif'

--- R/encode.R
+++ R/encode.R
@@ -2,10 +2,10 @@
 #'
 #' Encodes an AVIF image from a nativeRaster object.
 #'
-#' @param nr A nativeRaster object
-#' @param quality The quality of the image
-#' @param speed The speed of processing
-#' @returns A raw vector containing the encoded image data
+#' @param nr A nativeRaster object.
+#' @param quality The quality of the image.
+#' @param speed The processing speed.
+#' @returns A raw vector containing the encoded image data.
 #' @export
 encode <- function(nr, quality = 80, speed = 10) {
   if (!inherits(nr, "nativeRaster")) {
@@ -18,4 +18,21 @@ encode <- function(nr, quality = 80, speed = 10) {
     stop("`speed` must be between 0 and 10")
   }
   encode_avif(nr, nrow(nr), ncol(nr), as.integer(quality), as.integer(speed))
+}
+
+#' Write an AVIF image to a file
+#'
+#' @inheritParams encode
+#' @param path The path to write the image.
+#' @returns
+#' If `path` is `NULL`, returns a raw vector containing the encoded image data.
+#' Otherwise, `path` is invisibly returned.
+#' @export
+write_avif <- function(nr, path = NULL, quality = 80, speed = 10) {
+  raw_bytes <- encode(nr, quality, speed)
+  if (is.null(path)) {
+    return(raw_bytes)
+  }
+  writeBin(raw_bytes, path)
+  invisible(path)
 }
